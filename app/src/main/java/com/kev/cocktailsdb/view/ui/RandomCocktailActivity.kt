a1d1a182867251c151716f7b3b8f4184838cd1b2
kev87ian <kelvinian87@gmail.com>
2022-08-11 01:37:38 +0300

    working on displaying ingredients and measurements dynamically

--- app/src/main/java/com/kev/cocktailsdb/view/ui/RandomCocktailActivity.kt
+++ app/src/main/java/com/kev/cocktailsdb/view/ui/RandomCocktailActivity.kt
@@ -1,8 +1,11 @@
 package com.kev.cocktailsdb.view.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -11,35 +14,40 @@ import com.kev.cocktailsdb.R
 import com.kev.cocktailsdb.data.model.Drink
 import com.kev.cocktailsdb.data.repository.RandomCocktailRepository
 import com.kev.cocktailsdb.util.Resource
-import com.kev.cocktailsdb.viewmodel.CocktailsViewModel
-import com.kev.cocktailsdb.viewmodel.MainViewModelProviderFactory
 import com.kev.cocktailsdb.viewmodel.RandomCocktailViewModel
 import com.kev.cocktailsdb.viewmodel.RandomCocktailViewModelProviderFactory
-import kotlinx.android.synthetic.main.activity_cocktails_details.*
 import kotlinx.android.synthetic.main.activity_random_cocktail.*
-import retrofit2.adapter.rxjava2.Result.response
+
 
 class RandomCocktailActivity : AppCompatActivity() {
-lateinit var viewModel: RandomCocktailViewModel
-lateinit var repository: RandomCocktailRepository
+    lateinit var viewModel: RandomCocktailViewModel
+    lateinit var repository: RandomCocktailRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random_cocktail)
 
+
+
+
+
+
         repository = RandomCocktailRepository()
 
-        val viewModelProviderFactory = RandomCocktailViewModelProviderFactory(application as HiltApplication, repository)
-        viewModel = ViewModelProvider(this, viewModelProviderFactory)[RandomCocktailViewModel::class.java]
+        val viewModelProviderFactory =
+            RandomCocktailViewModelProviderFactory(application as HiltApplication, repository)
+        viewModel =
+            ViewModelProvider(this, viewModelProviderFactory)[RandomCocktailViewModel::class.java]
 
-        viewModel.randomCocktail.observe(this, Observer {response->
+        viewModel.randomCocktail.observe(this, Observer { response ->
 
-            when(response){
-                is Resource.Error ->{
+            when (response) {
+                is Resource.Error -> {
 
 
                 }
 
-                is Resource.Success ->{
+                is Resource.Success -> {
                     response.data?.let {
 
                         val drink = it.drinks[0]
@@ -48,7 +56,7 @@ lateinit var repository: RandomCocktailRepository
 
                 }
 
-                is Resource.Loading->{
+                is Resource.Loading -> {
 
                 }
             }
@@ -57,8 +65,35 @@ lateinit var repository: RandomCocktailRepository
 
     private fun setUI(drink: Drink) {
 
-        Glide.with(baseContext).load(drink.strDrinkThumb).placeholder(R.drawable.loading).into(randomCocktailImageView)
-        randomNameTV.text = drink.strDrink
+        Glide.with(baseContext).load(drink.strDrinkThumb).placeholder(R.drawable.loading)
+            .into(randomCocktailImageView)
+        randomCocktailNameTv.text = drink.strDrink
         Toast.makeText(this, drink.strDrink, Toast.LENGTH_LONG).show()
+
+
+        val ingredientList = arrayOf(
+            drink.strIngredient1,
+            drink.strIngredient2,
+            drink.strIngredient3,
+            drink.strIngredient4,
+            drink.strIngredient5
+        )
+
+        val measurementLis: Array<String> = arrayOf(
+            drink.strMeasure1,
+            drink.strMeasure2,
+            drink.strMeasure3,
+            drink.strMeasure4,
+            drink.strMeasure5
+        )
+
+
+        for (i in ingredientList.size..25) {
+            val dynamicTextview = TextView(this)
+           tvMeasurements.text = ingredientList[0]
+
+        }
     }
+
+
 }
