358fe6046cc5aeb877d7f84ab1b8f92243dff978
kev87ian <kelvinian87@gmail.com>
2022-08-11 15:31:46 +0300

    UI changes

--- app/src/main/java/com/kev/cocktailsdb/view/ui/RandomCocktailActivity.kt
+++ app/src/main/java/com/kev/cocktailsdb/view/ui/RandomCocktailActivity.kt
@@ -1,12 +1,9 @@
 package com.kev.cocktailsdb.view.ui
 
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.kev.cocktailsdb.HiltApplication
@@ -39,15 +36,19 @@ class RandomCocktailActivity : AppCompatActivity() {
         viewModel =
             ViewModelProvider(this, viewModelProviderFactory)[RandomCocktailViewModel::class.java]
 
-        viewModel.randomCocktail.observe(this, Observer { response ->
+        viewModel.randomCocktail.observe(this) { response ->
 
             when (response) {
                 is Resource.Error -> {
 
 
+                    randomCocktailsProgressBar.visibility = View.GONE
+                    errorRandomCocktail.text = response.message
+                    errorRandomCocktail.visibility = View.VISIBLE
                 }
 
                 is Resource.Success -> {
+                    randomCocktailsProgressBar.visibility = View.GONE
                     response.data?.let {
 
                         val drink = it.drinks[0]
@@ -57,10 +58,10 @@ class RandomCocktailActivity : AppCompatActivity() {
                 }
 
                 is Resource.Loading -> {
-
+                    randomCocktailsProgressBar.visibility = View.VISIBLE
                 }
             }
-        })
+        }
     }
 
     private fun setUI(drink: Drink) {
@@ -70,30 +71,105 @@ class RandomCocktailActivity : AppCompatActivity() {
         randomCocktailNameTv.text = drink.strDrink
         Toast.makeText(this, drink.strDrink, Toast.LENGTH_LONG).show()
 
-
-        val ingredientList = arrayOf(
-            drink.strIngredient1,
-            drink.strIngredient2,
-            drink.strIngredient3,
-            drink.strIngredient4,
-            drink.strIngredient5
-        )
-
-        val measurementLis: Array<String> = arrayOf(
-            drink.strMeasure1,
-            drink.strMeasure2,
-            drink.strMeasure3,
-            drink.strMeasure4,
-            drink.strMeasure5
-        )
+        randomCocktailDetailsCategoryTV.text = drink.strAlcoholic
+        randomCocktailGlassTv.text = drink.strGlass
+        randomCocktailInstructionsTv.text = drink.strInstructions
 
 
-        for (i in ingredientList.size..25) {
-            val dynamicTextview = TextView(this)
-           tvMeasurements.text = ingredientList[0]
-
+        if (!drink.strIngredient1.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient1))
         }
+
+
+        if (!drink.strIngredient2.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient2))
+        }
+
+        if (!drink.strIngredient3.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient3))
+        }
+
+        if (!drink.strIngredient4.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient4))
+        }
+
+        if (!drink.strIngredient5.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient5))
+        }
+        if (!drink.strIngredient6.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient6))
+        }
+        if (!drink.strIngredient7.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient7))
+        }
+        if (!drink.strIngredient8.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient8))
+        }
+        if (!drink.strIngredient9.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient9))
+        }
+        if (!drink.strIngredient10.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient10))
+        }
+        if (!drink.strIngredient11.isNullOrEmpty()) {
+            drinkDetailIngredient1Tv.append("\n \u2022 ".plus(drink.strIngredient11))
+        }
+
+
+
+
+
+        if (!drink.strMeasure1.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure1))
+        }
+
+        if (!drink.strMeasure2.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure2))
+        }
+
+        if (!drink.strMeasure3.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure3))
+        }
+
+        if (!drink.strMeasure4.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure4))
+        }
+
+        if (!drink.strMeasure5.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure5))
+        }
+
+
+        if (!drink.strMeasure6.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure6))
+        }
+
+
+        if (!drink.strMeasure7.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure7))
+        }
+
+
+        if (!drink.strMeasure8.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure8))
+        }
+
+
+        if (!drink.strMeasure9.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure9))
+        }
+
+
+        if (!drink.strMeasure10.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure10))
+        }
+
+
+        if (!drink.strMeasure11.isNullOrEmpty()) {
+            drinkDetailMeasurement1Tv.append("\n : ".plus(drink.strMeasure11))
+        }
+
+
+
     }
-
-
 }
